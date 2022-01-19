364754c897c94405afcfd884765ad837e835fbd5
jjlorenzo <jjlorenzo@users.noreply.github.com>
2022-01-19 10:46:42 -0500

    feat(endpoints): add analytics Event endpoints
    refs #3

--- src/acme/service/oculus/api/analytics/event.py
+++ src/acme/service/oculus/api/analytics/event.py
@@ -1,4 +1,6 @@
+from ... import models
 from rest_framework import response
+from rest_framework import serializers
 from rest_framework import status
 from rest_framework import viewsets
 
@@ -6,4 +8,31 @@ from rest_framework import viewsets
 class ViewSet(viewsets.ViewSet):
 
   def list(self, request):
-    return response.Response([], status=status.HTTP_200_OK)
+    filters = {}
+    ordering = []
+    if category := request.query_params.get("category"):
+      filters["category"] = serializers.CharField().to_internal_value(category)
+    if session := request.query_params.get("session"):
+      filters["session"] = serializers.CharField().to_internal_value(session)
+      ordering.append("timestamp")
+    if timestamp_after := request.query_params.get("timestamp-after"):
+      filters["timestamp__gte"] = serializers.DateTimeField().to_internal_value(timestamp_after)
+    if timestamp_before := request.query_params.get("timestamp-before"):
+      filters["timestamp__lte"] = serializers.DateTimeField().to_internal_value(timestamp_before)
+    queryset = models.Event.objects.filter(**filters).order_by(*ordering)
+
+    serializer = ListSerializer(
+      queryset,
+      many=True,
+      context={},
+    )
+    return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ListSerializer(serializers.Serializer):
+  id = serializers.IntegerField()
+  name = serializers.CharField()
+  payload = serializers.JSONField()
+  category = serializers.CharField()
+  session = serializers.CharField()
+  timestamp = serializers.DateTimeField()
