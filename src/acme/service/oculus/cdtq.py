6bca0ec67f71fe82c38b336efb5cb201c129ae5f
jjlorenzo <jjlorenzo@users.noreply.github.com>
2022-01-19 10:48:55 -0500

    fix(celery): separate celery settings from django.conf

--- src/acme/service/oculus/cdtq.py
+++ src/acme/service/oculus/cdtq.py
@@ -3,6 +3,7 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acme.service.oculus.conf")
 
-application = celery.Celery("oculus")
-
-application.config_from_object("django.conf:settings", namespace="CELERY")
+application = celery.Celery(
+  broker="amqp://guest:guest@localhost//",
+  main="oculus",
+)
